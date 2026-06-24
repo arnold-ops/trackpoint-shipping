0e3021b75ce7edf8a7b1c53360d1738d6a2981ac
arnold-ops <115327083+arnold-ops@users.noreply.github.com>
2026-06-24 13:33:40 +0000

    update: site modifications and bug fixes

--- js/supabase-config.js
+++
@@ -1,5 +0,0 @@
-const SUPABASE_URL = 'https://kvooglyzbljhslqwtklx.supabase.co'
-const SUPABASE_ANON_KEY = 'sb_publishable_NEo_Gp66dJ1c7wZXWXeUYQ_m3JkhgfP'
-
-const { createClient } = supabase
-const db = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
